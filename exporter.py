88eac8143a30c71ab865c8c07ba4f9d87fca2051
Francisco Mendes <francisco.a.mendes.98@gmail.com>
2022-04-29 01:53:20 +0100

    Refactor Excel Exporter Script
    Use Xlwings
    Use Proper file name and location
    Remove parameters from docs
    Refactor offsetting

--- exporter.py
+++ exporter.py
@@ -1,9 +1,8 @@
-import datetime
+from datetime import date as Date
 import sys
 from pathlib import Path
 from typing import Union
-
-import openpyxl
+import xlwings as xl
 
 sheet_name: str = open('sheet_name.secret.txt', encoding='utf-8').readline()
 
@@ -12,43 +11,39 @@ def export_to_excel(report_dir: str, excel_dir: str):
     """
     Reads the report files and writes each of them into the excel sheets.
     Assumes that the reports are from this month.
-
-    :param report_dir: The directory containing the report files in raw (*.txt) format
-    :param excel_dir: The directory containing the excel spreadsheets to be written to
     """
-    for report_path in Path(report_dir).glob('*.txt'):
-        print("report name:", report_path)
-        records = read_report(report_path)
-        excel_path = Path(excel_dir) / report_path.stem
-        excel_path = excel_path.with_suffix('.xlsx')
-        print("excel name:", excel_path)
-        write_report(excel_path, records)
+    with xl.App(add_book=False) as app:
+        for report_path in Path(report_dir).glob('*.txt'):
+            station = report_path.stem
+            print('processing', station)
+            excel_path = Path(excel_dir) / station / f'Registos_de_Producao_PV_{station}.xlsx'
+
+            records = read_report(report_path)
+            write_report(app, excel_path, records)
 
 
 def read_report(report_path: Path) -> list[str]:
     """
     Reads the report file and returns its list of records.
-    :param report_path: The path of the report file.
-    :return: The records within
     """
     with open(report_path) as report_file:
         return [line.rstrip() for line in report_file]
 
 
-def write_report(excel_path: Path, records: list[str]):
+def write_report(app: xl.App, excel_path: Path, records: list[str], date: Date = Date.today()):
     """
-    Writes the records to the excel spreadsheet.
-    Assumes that the records are from the present month.
-    :param excel_path: The path of the excel file.
-    :param records: The records to write.
-    """
-    wb = openpyxl.load_workbook(str(excel_path))
-    sheet = wb[sheet_name]
+    Writes the records to the excel spreadsheet, according to the given date, defaulting to today.
 
-    column = offset_month(datetime.datetime.now())
-    for index, record in enumerate(records):
-        row = offset_day(index)
-        sheet.cell(row=row, column=column, value=to_float(record))
+    """
+    options = {'update_links': True, 'ignore_read_only_recommended': True, 'editable': True}
+    wb: xl.Book = app.books.open(excel_path, **options)
+    sheet: xl.Sheet = wb.sheets[sheet_name]
+    sheet.activate()
+
+    column = offset_month(date)
+    start, end = offset_day(records)
+    cells: xl.Range = sheet.range((start, column), (end, column))
+    cells.value = [[to_float(v)] for v in records]
 
     wb.save(excel_path.with_name(excel_path.stem + 'uwu.xlsx'))
 
@@ -56,8 +51,6 @@ def write_report(excel_path: Path, records: list[str]):
 def to_float(value: str) -> Union[float | None]:
     """
     Parses a float from a str or returns None if parsing fails.
-    :param value: The string to parse
-    :return: the corresponding float or None
     """
     try:
         return float(value)
@@ -65,20 +58,16 @@ def to_float(value: str) -> Union[float | None]:
         return None
 
 
-def offset_day(index: int) -> int:
+def offset_day(records: list) -> tuple[int, int]:
     """
-    Offset for the row of the spreadsheet.
-    :param index: The index of the record.
-    :return: The row of the spreadsheet.
+    Creates offsets for insertion into the worksheet for the given list of records.
     """
-    return index + 13
+    return 13, len(records) + 12
 
 
-def offset_month(date: datetime.date) -> int:
+def offset_month(date: Date) -> int:
     """
     Offset for the column of the spreadsheet, based on the date's month and year.
-    :param date: The date to use as an index.
-    :return: The column of the spreadsheet.
     """
     year = (date.year - 2021) * 12
     months = (year + date.month - 1) * 3
